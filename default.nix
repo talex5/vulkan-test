9957794fc6515f50fa4981b6c810d05a3276d353
Thomas Leonard <talex5@gmail.com>
2025-09-11 09:31:17 +0100

    Improve Nix flake
    - Remove unused glfw3 dependency
    - Use fish instead of bash
    - Only include validation layers in the dev shell

--- default.nix
+++ default.nix
@@ -1,19 +1,17 @@
-{ pkgs, glfw3, pkgconf, shaderc, vulkan-headers, vulkan-loader, vulkan-validation-layers, wayland, libffi,
+{ pkgs, pkgconf, shaderc, vulkan-headers, vulkan-loader, vulkan-validation-layers, wayland, libffi,
   wayland-protocols, wayland-scanner, libdrm, linuxHeaders }:
 
 pkgs.stdenv.mkDerivation {
-  name = "hello-3d";
+  name = "vulkan-test-c";
 
   src = ./.;
 
   buildInputs = [
-    pkgconf glfw3 shaderc vulkan-headers vulkan-loader vulkan-validation-layers wayland libffi
+    pkgconf shaderc vulkan-headers vulkan-loader vulkan-validation-layers wayland libffi
     wayland-protocols wayland-scanner libdrm
   ];
 
   LINUX_HEADERS = linuxHeaders;
-
-  VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
 
   # Use $CC as it allows for stdenv to reference the correct C compiler
   buildPhase = ''make'';
--- flake.nix
+++ flake.nix
@@ -6,7 +6,16 @@
   };
 
   outputs = { self, nixpkgs }:
-  let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
-    packages.x86_64-linux.default = pkgs.callPackage (import ./default.nix) {};
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system}; in {
+    packages.${system}.default = pkgs.callPackage (import ./default.nix) {};
+
+    devShells.${system}.default = pkgs.mkShell {
+      VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+
+      buildInputs = self.outputs.packages.${system}.default.buildInputs;
+      shellHook = ''exec ${pkgs.fish}/bin/fish'';
+    };
   };
 }
